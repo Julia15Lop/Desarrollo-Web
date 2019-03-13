317695e2654b5ad5aab23a02917b0fdaecd3dd53
angelloro <angel.loro@alu.uclm.es>
2019-03-13 18:57:43 +0100

    edit notes

--- wsdevelopment/app/controllers/notes_controller.rb
+++ wsdevelopment/app/controllers/notes_controller.rb
@@ -3,11 +3,23 @@ class NotesController < ApplicationController
     	@note = Note.all
   	end
 	def show
-    	@note = Note.find(params[:id])
+    		@note = Note.find(params[:id])
   	end
 
 	def new
+		@note=Note.new
 	end
+
+	def update
+  	@note = Article.find(params[:id])
+ 
+  	if @note.update(note_params)
+    		redirect_to @note
+  	else
+    		render 'edit'
+  		end
+	end
+
 
 	def create
 		@note = Note.new(note_params)
