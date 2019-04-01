4f53b173979f5989b68baadd52bc56e3f47be56b
JuliaUCLM <julia.lopez4@alu.uclm.es>
2019-04-01 20:09:36 +0200

    User Logout

--- wsdevelopment/config/routes.rb
+++ wsdevelopment/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'logout' => 'session#destroy', :as => 'logout'
+  get 'login' => 'session#new', :as => 'login'
+  post 'login' => 'session#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     resources :notes
