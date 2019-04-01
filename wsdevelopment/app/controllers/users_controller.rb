693d066fe24e6ee16fb6718da18b0417d7a9853f
angelloro <angel.loro@alu.uclm.es>
2019-04-01 19:23:07 +0200

    edit users completed

--- wsdevelopment/app/controllers/users_controller.rb
+++ wsdevelopment/app/controllers/users_controller.rb
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @user = User.new
   end
