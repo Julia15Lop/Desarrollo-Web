52942a3b0c771206c61ae78983001826c7397e79
JuliaUCLM <julia.lopez4@alu.uclm.es>
2019-04-21 19:33:55 +0200

    Add link to edit

--- wsdevelopment/app/helpers/layouts_helper.rb
+++ wsdevelopment/app/helpers/layouts_helper.rb
@@ -1,7 +1,7 @@
 module LayoutsHelper
-	# def user_id(user_name)
-	# 	User.where(user_name: user_name).first.id
-	# end
+	def user_id(user_name)
+		User.where(user_name: user_name).first.id
+	end
 
 	def user_name(user_id)
 		User.where(id: user_id).first
