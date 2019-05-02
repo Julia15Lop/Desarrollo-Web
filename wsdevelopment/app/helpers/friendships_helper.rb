783bb3c55d62a9854ee2ff92e69229ab6ac36c1d
JuliaUCLM <julia.lopez4@alu.uclm.es>
2019-05-02 19:34:47 +0200

    Confirmed and Pending requests

--- wsdevelopment/app/helpers/friendships_helper.rb
+++ wsdevelopment/app/helpers/friendships_helper.rb
@@ -1,11 +1,30 @@
 module FriendshipsHelper
 
-	def get_friendships(user_id, status)
-		friendships = Friendship.where(user_id: user_id, status: status)
+	def get_friendships(friend_id, status)
+		if status.eql? 'pending'
+			friendships = Friendship.where(friend_id: friend_id, status: status)
+		else status.eql? 'confirmed'
+			friendships = Friendship.where(user_id: friend_id).or(Friendship.where(friend_id: friend_id)).where(status: status)
+		end
 	end
 
-	def find_user(user_id)
-  		User.where(id: user_id).first
+	def get_new_friendships(user_id)
+		users_ids = Friendship.where(user_id: user_id).pluck(:friend_id)
+		u = Friendship.where(friend_id: user_id).pluck(:user_id)
+		users_ids = users_ids + u unless u.empty?
+		users_ids << user_id
+		
+		users = get_all_users_except(users_ids)
+
+	end
+
+	def get_all_users_except(id)
+		User.where.not(id: id)
+	end
+	def find_user(user_name, friendship)
+		user = User.where(user_name: user_name).first
+		friendship.user_id == user.id ? id = friendship.friend_id : id = friendship.user_id
+  		User.find(id)
   	end
 
   	def logged?
