ab918a6cf5f025abd71bbfe568fea94c02ae8172
JuliaUCLM <julia.lopez4@alu.uclm.es>
2019-05-05 22:09:04 +0200

    changes in database

--- wsdevelopment/app/helpers/users_helper.rb
+++ wsdevelopment/app/helpers/users_helper.rb
@@ -10,6 +10,9 @@ module UsersHelper
   	user = User.where(id: user_id).first
   end
 
+  def get_user_id(user_name)
+    user = User.find_by(user_name: user_name).id
+  end
   def logged?
     session[:user_name]
   end
